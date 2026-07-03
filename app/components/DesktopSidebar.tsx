960fd800340c2eccbb693a855d38f2995e6fe504
ImperioKDB <kingbrite123@gmail.com>
2026-07-03 06:31:39 +0100

    feat: wire Members into desktop sidebar nav

--- app/components/DesktopSidebar.tsx
+++ app/components/DesktopSidebar.tsx
@@ -15,6 +15,7 @@ const ICONS: Record<string, string> = {
   reports:      'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z',
   wallets:      'M21 12a2 2 0 00-2-2H5a2 2 0 00-2 2m18 0v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6m18 0V9a2 2 0 00-2-2H5a2 2 0 00-2 2v3m18 0h-5.5a1.5 1.5 0 000 3H21',
   about:        'M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
+  members:      'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z',
 }
 
 export default function DesktopSidebar({ university }: Props) {
@@ -25,6 +26,7 @@ export default function DesktopSidebar({ university }: Props) {
     { key: 'overview',     label: 'Overview',     href: base },
     { key: 'transactions', label: 'Transactions', href: '/transactions' },
     { key: 'proposals',    label: 'Proposals',    href: '/proposals?treasury=' + university },
+    { key: 'members',      label: 'Members',      href: base + '/members' },
     { key: 'signatures',   label: 'Signatures',   href: null as string | null },
     { key: 'reports',      label: 'Reports',      href: null as string | null },
     { key: 'wallets',      label: 'Wallets',      href: null as string | null },
